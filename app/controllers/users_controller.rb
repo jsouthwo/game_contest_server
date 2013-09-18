8ec0bdc97c5ba6bcde90be02aada740516a51be8
Justin Southworth <justin_southworth@taylor.edu>
2013-09-18 13:05:02 -0400

    After class on Wed., 9-18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        # TODO ASSIGNMENT IS TO TRY TO FIGURE OUT WHY THIS DOESN'T WORK!!!
+        #@user = User.new(params[:user])
+        #@user = User.new(do something..), we'd want it to look like ^, but that shouldn't work.
+
+=begin
+        @user.save
+=end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130917182641) do
+ActiveRecord::Schema.define(version: 20130918163321) do
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password"
     t.string   "username"
+    t.string   "password_digest"
   end
 
 end
