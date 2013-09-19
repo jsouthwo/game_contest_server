8438fe6b9bb241d130d1de35e33da86701ec6597
Justin Southworth <justin_southworth@taylor.edu>
2013-09-19 14:54:51 -0400

    Solved hw for Friday, 9-19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,9 @@ class UsersController < ApplicationController
         # TODO ASSIGNMENT IS TO TRY TO FIGURE OUT WHY THIS DOESN'T WORK!!!
         #@user = User.new(params[:user])
         #@user = User.new(do something..), we'd want it to look like ^, but that shouldn't work.
+        # http://guides.rubyonrails.org/getting_started.html
+        #   search for strong_parameters
+        #   http://weblog.rubyonrails.org/2012/3/21/strong-parameters/
 
 =begin
         @user.save
