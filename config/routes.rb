e3e9c66df76734e8c9d8269836ec020f2b69ce9e
Justin Southworth <justin_southworth@taylor.edu>
2013-10-11 12:55:24 -0400

    Using more ruby.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 GameContestServer::Application.routes.draw do
+    root 'users#index'
     get "users/new"
     get "users/create"
     resources :users 
     get 'signup', to: 'users#new', as: 'signup'
-    get 'root', to: 'users#new', as: 'root'
     # The priority is based upon order of creation: first created -> highest priority.
     # See how all your routes lay out with "rake routes".
 
