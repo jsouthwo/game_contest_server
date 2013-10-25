1ba25ca559d286e1a8eeb688e5c0e07f2ec17fd8
Justin Southworth <justin_southworth@taylor.edu>
2013-10-24 20:33:58 -0400

    Copied his tests since I screwed up the merge.

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -1,128 +1,135 @@
 require 'spec_helper'
 
 describe "AuthenticationPages" do
-    subject { page }
-    describe "login page" do
-        before { visit login_path }
+  subject { page }
+  describe "login page" do
+    before { visit login_path }
 
-        it { should have_content('Log In') }
+    it { should have_content('Log In') }
 
-        describe "with invalid account" do
-            before { click_button 'Log In' }
+    describe "with invalid account" do
+      before { click_button 'Log In' }
 
       it { should have_alert(:danger, text: 'Invalid') }
 
-            describe "visiting another page" do
-                before { click_link 'Home' }
+      describe "visiting another page" do
+    before { click_link 'Home' }
 
-	it { should_not have_alert(:danger) }
+    it { should_not have_alert(:danger) }
       end
     end
 
     describe "with valid account" do
       let(:user) { FactoryGirl.create(:user) }
 
-            before do
-                fill_in 'Username', with: user.username
-                fill_in 'Password', with: user.password
-                click_button 'Log In'
-            end
+      before do
+    fill_in 'Username', with: user.username
+    fill_in 'Password', with: user.password
+    click_button 'Log In'
+      end
 
-            it { should have_link('Profile', href: user_path(user)) }
-            it { should have_link('Settings', href: edit_user_path(user)) }
-            it { should have_link('Log Out', href: logout_path) }
-            it { should_not have_link('Log In', href: login_path) }
-            it { should_not have_link('Sign Up', href: signup_path) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Log Out', href: logout_path) }
+      it { should_not have_link('Log In', href: login_path) }
+      it { should_not have_link('Sign Up', href: signup_path) }
 
       it { should have_alert(:success) }
 
-            describe "followed by logout" do
-                before { click_link 'Log Out' }
+      describe "followed by logout" do
+    before { click_link 'Log Out' }
 
-                it { should have_link('Log In', href: login_path) }
-                it { should have_link('Sign Up', href: signup_path) }
-                it { should_not have_link('Log Out', href: logout_path) }
-                it { should_not have_link('Profile') }
+    it { should have_link('Log In', href: login_path) }
+    it { should have_link('Sign Up', href: signup_path) }
+    it { should_not have_link('Log Out', href: logout_path) }
+    it { should_not have_link('Profile') }
 
-	it { should have_alert(:info) }
+    it { should have_alert(:info) }
       end
     end
+  end
 end
 
 describe "AuthorizationPages" do
-    subject { page }
+  subject { page }
 
-    let(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user) }
 
-    describe "non-authenticated users" do
-        describe "for Users controller" do
-            describe "edit action" do
-                before { visit edit_user_path(user) }
+  describe "non-authenticated users" do
+    describe "for Users controller" do
+      describe "edit action" do
+    before { visit edit_user_path(user) }
 
-	it { should have_alert(:warning) }
-	it { should have_content('Log In') }
+    it { should have_alert(:warning) }
+    it { should have_content('Log In') }
       end
 
-            describe "update action", type: :request do
-                before { patch user_path(user) }
+      describe "update action", type: :request do
+    before { patch user_path(user) }
 
-	it { errors_on_redirect(login_path, :warning) }
+    it { errors_on_redirect(login_path, :warning) }
       end
     end
+  end
 
-    describe "authenticated users" do
-        before { login user, avoid_capybara: true }
+  describe "authenticated users" do
+    before { login user, avoid_capybara: true }
 
-        describe "new action", type: :request do
-            before { get new_user_path }
+    describe "new action", type: :request do
+      before { get new_user_path }
 
       it { errors_on_redirect(root_path, :warning) }
     end
 
-        describe "create action", type: :request do
-            before { post users_path }
+    describe "create action", type: :request do
+      before { post users_path }
 
       it { errors_on_redirect(root_path, :warning) }
     end
+  end
 
-    describe "authenticated, but wrong user" do
-        let(:other_user) { FactoryGirl.create(:user) }
+  describe "authenticated, but wrong user" do
+    let(:other_user) { FactoryGirl.create(:user) }
 
-        before { login user, avoid_capybara: true }
+    before { login user, avoid_capybara: true }
 
-        describe "edit action", type: :request do
-            before { get edit_user_path(other_user) }
+    describe "edit action", type: :request do
+      before { get edit_user_path(other_user) }
 
       specify { expect(response.body).not_to match('Edit user') }
       it { errors_on_redirect(root_path, :danger) }
     end
 
-        describe "update action", type: :request do
-            before { patch user_path(other_user) }
+    describe "update action", type: :request do
+      before { patch user_path(other_user) }
 
       it { errors_on_redirect(root_path, :danger) }
     end
+  end
 
-    describe "authenticated, but non-admin user" do
-        let(:other_user) { FactoryGirl.create(:user) }
+  describe "authenticated, but non-admin user" do
+    let(:other_user) { FactoryGirl.create(:user) }
 
-        before { login user, avoid_capybara: true }
+    before { login user, avoid_capybara: true }
 
-        describe "update action", type: :request do
-            before { patch user_path(other_user) }
-
-      it { errors_on_redirect(root_path, :danger) }
-    end
-
-    describe "authenticated admin user" do
-        describe "delete action (self)", type: :request do
-            let(:admin) { FactoryGirl.create(:admin) }
-
-            before do
-                login admin, avoid_capybara: true
-                delete user_path(admin)
-            end
+    describe "update action", type: :request do
+      before { patch user_path(other_user) }
 
       it { errors_on_redirect(root_path, :danger) }
     end
+  end
+
+  describe "authenticated admin user" do
+    describe "delete action (self)", type: :request do
+      let(:admin) { FactoryGirl.create(:admin) }
+
+      before do
+    login admin, avoid_capybara: true
+    delete user_path(admin)
+      end
+
+      it { errors_on_redirect(root_path, :danger) }
+    end
+  end
 end
+
--- spec/features/users_pages_spec.rb
+++ spec/features/users_pages_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe "UsersPages" do
-    subject { page }
+  subject { page }
 
-    describe "Sign Up" do
-        let(:submit) { 'Create new account' }
+  describe "Sign Up" do
+    let(:submit) { 'Create new account' }
 
-        before { visit signup_path }
+    before { visit signup_path }
 
     describe "passwords are not visible when typing" do
       it { should have_field 'user_password', type: 'password' }
@@ -15,21 +15,21 @@ describe "UsersPages" do
 
     describe "with invalid information" do
       it "does not add the user to the system" do
-	expect { click_button submit }.not_to change(User, :count)
+    expect { click_button submit }.not_to change(User, :count)
       end
 
       it "produces an error message" do
-	click_button submit
-	should have_alert(:danger)
+    click_button submit
+    should have_alert(:danger)
       end
     end
 
     describe "with valid information" do
       before do
-	fill_in 'Username', with: 'User Name'
-	fill_in 'Email', with: 'user@example.com'
-	fill_in 'Password', with: 'password'
-	fill_in 'Confirmation', with: 'password'
+    fill_in 'Username', with: 'User Name'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirmation', with: 'password'
       end
 
       it "allows the user to fill in user fields" do
@@ -37,21 +37,26 @@ describe "UsersPages" do
       end
 
       describe "redirects properly", type: :request do
-	before do
-	  post users_path, user: { username: 'User Name',
-				   email: 'user@example.com',
-				   password: 'password',
-				   password_confirmation: 'password' }
+    before do
+      post users_path, user: { username: 'User Name',
+                   email: 'user@example.com',
+                   password: 'password',
+                   password_confirmation: 'password' }
         end
 
-        describe "with invalid information" do
-            it "does not add the user to the system" do
-                expect { click_button submit }.not_to change(User, :count)
-            end
+    specify { expect(response).to redirect_to(user_path(assigns(:user))) }
+      end
 
-	it { should have_link('Log Out') }
-	it { should_not have_link('Log In') }
-	it { should have_alert(:success, text: 'Welcome') }
+      it "adds a new user to the system" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after creating the user" do
+    before { click_button submit }
+
+    it { should have_link('Log Out') }
+    it { should_not have_link('Log In') }
+    it { should have_alert(:success, text: 'Welcome') }
       end
     end
   end
@@ -68,26 +73,31 @@ describe "UsersPages" do
       it { should_not have_content(user.password_digest) }
     end
 
-    describe "Display Users" do
-        describe "individually" do
-            let(:user) { FactoryGirl.create(:user) }
+    describe "all" do
+      before(:all) { 25.times { FactoryGirl.create(:user) } }
+      after(:all) { User.all.each { |user| user.destroy } }
 
-            before { visit user_path(user) }
+      before(:each) { visit users_path }
 
-            it { should have_content(user.username) }
-            it { should have_content(user.email) }
-            it { should_not have_content(user.password) }
-            it { should_not have_content(user.password_digest) }
-        end
+      it { should have_content('List of users') }
+      it { should have_content('25 users') }
 
-        describe "all" do
-            before(:all) { 25.times { FactoryGirl.create(:user) } }
-            after(:all) { User.all.each { |user| user.destroy } }
+      # fix up with pagination later...
+      User.all.each do |user|
+    it { should have_selector('li', text: user.username) }
+      end
+    end
+  end
 
-            before(:each) { visit users_path }
+  describe "Edit users" do
+    let (:user) { FactoryGirl.create(:user) }
+    let!(:orig_username) { user.username }
+    let (:submit) { 'Update account' }
 
-            it { should have_content('List of users') }
-            it { should have_content('25 users') }
+    before do
+      login user
+      visit edit_user_path(user)
+    end
 
     it { should have_field('Username', with: user.username) }
     it { should have_field('Email', with: user.email) }
@@ -95,10 +105,10 @@ describe "UsersPages" do
 
     describe "with invalid information" do
       before do
-	fill_in 'Username', with: ''
-	fill_in 'Email', with: user.email
-	fill_in 'Password', with: user.password
-	fill_in 'Confirmation', with: user.password
+    fill_in 'Username', with: ''
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Confirmation', with: user.password
       end
 
       describe "does not change data" do
@@ -113,150 +123,110 @@ describe "UsersPages" do
       end
 
       it "produces an error message" do
-	click_button submit
-	should have_alert(:danger)
+    click_button submit
+    should have_alert(:danger)
       end
     end
 
-    describe "Edit users" do
-        let (:user) { FactoryGirl.create(:user) }
-        let!(:orig_username) { user.username }
-        let (:submit) { 'Update account' }
+    describe "with forbidden attributes", type: :request do
+      before do
+    login user, avoid_capybara: true
+    patch user_path(user), user: { admin: true,
+                       password: user.password,
+                       password_confirmation: user.password }
+      end
 
-        before do
-            login user
-            visit edit_user_path(user)
+      specify { expect(user.reload).not_to be_admin }
+    end
+
+    describe "with valid information" do
+      before do
+    fill_in 'Username', with: 'Changed name'
+    fill_in 'Email', with: 'new@example.com'
+    fill_in 'Password', with: user.password
+    fill_in 'Confirmation', with: user.password
+      end
+
+      describe "changes the data" do
+        before { click_button submit }
+
+        specify { expect(user.reload.username).to eq('Changed name') }
+        specify { expect(user.reload.email).to eq('new@example.com') }
+      end
+
+      describe "redirects properly", type: :request do
+    before do
+      login user, avoid_capybara: true
+      patch user_path(user), user: { username: 'Changed name',
+                     email: user.email,
+                     password: user.password,
+                     password_confirmation: user.password }
         end
 
-        it { should have_field('Username', with: user.username) }
-        it { should have_field('Email', with: user.email) }
-        it { should_not have_field('Password', with: user.password) }
-
-        describe "with invalid information" do
-            before do
-                fill_in 'Username', with: ''
-                fill_in 'Email', with: user.email
-                fill_in 'Password', with: user.password
-                fill_in 'Confirmation', with: user.password
-            end
-
-            describe "does not change data" do
-                before { click_button submit }
-
-                specify { expect(user.reload.username).not_to eq('') }
-                specify { expect(user.reload.username).to eq(orig_username) }
-            end
-
-            it "does not add a new user to the system" do
-                expect { click_button submit }.not_to change(User, :count)
-            end
-
-            it "produces an error message" do
-                click_button submit
-                should have_selector('div.alert.alert-danger')
-            end
-        end
-
-        describe "with forbidden attributes", type: :request do
-            before do
-                login user, avoid_capybara: true
-                patch user_path(user), user: { admin: true,
-                    password: user.password,
-                    password_confirmation: user.password }
-            end
+    specify { expect(response).to redirect_to(user_path(user)) }
+      end
 
       it "produces an update message" do
-	click_button submit
-	should have_alert(:success)
+    click_button submit
+    should have_alert(:success)
       end
 
-        describe "with valid information" do
-            before do
-                fill_in 'Username', with: 'Changed name'
-                fill_in 'Email', with: 'new@example.com'
-                fill_in 'Password', with: user.password
-                fill_in 'Confirmation', with: user.password
-            end
+      it "does not add a new user to the system" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+  end
 
-            describe "changes the data" do
-                before { click_button submit }
+  describe "Delete users" do
+    describe "as anonymous" do
+      let!(:user) { FactoryGirl.create(:user) }
 
-                specify { expect(user.reload.username).to eq('Changed name') }
-                specify { expect(user.reload.email).to eq('new@example.com') }
-            end
+      before { visit users_path }
 
-            describe "redirects properly", type: :request do
-                before do
-                    login user, avoid_capybara: true
-                    patch user_path(user), user: { username: 'Changed name',
-                        email: user.email,
-                        password: user.password,
-                        password_confirmation: user.password }
-                end
-
-                specify { expect(response).to redirect_to(user_path(user)) }
-            end
-
-            it "produces an update message" do
-                click_button submit
-                should have_selector('div.alert.alert-success')
-            end
-
-            it "does not add a new user to the system" do
-                expect { click_button submit }.not_to change(User, :count)
-            end
-        end
+      it { should_not have_link('delete') }
     end
 
-    describe "Delete users" do
-        describe "as anonymous" do
-            let!(:user) { FactoryGirl.create(:user) }
+    describe "as a user" do
+      let (:user) { FactoryGirl.create(:user) }
 
-            before { visit users_path }
+      before do
+    login user
+    visit users_path
+      end
 
-            it { should_not have_link('delete') }
-        end
+      it { should_not have_link('delete') }
+    end
 
-        describe "as a user" do
-            let (:user) { FactoryGirl.create(:user) }
+    describe "as admin" do
+      let (:admin) { FactoryGirl.create(:admin) }
+      let!(:user) { FactoryGirl.create(:user) }
 
-            before do
-                login user
-                visit users_path
-            end
+      before do
+        login admin
+        visit users_path
+      end
 
-            it { should_not have_link('delete') }
-        end
+      it { should have_link('delete', href: user_path(user)) }
+      it { should_not have_link('delete', href: user_path(admin)) }
 
-        describe "as admin" do
-            let (:admin) { FactoryGirl.create(:admin) }
-            let!(:user) { FactoryGirl.create(:user) }
+      describe "redirects properly", type: :request do
+    before do
+      login admin, avoid_capybara: true
+      delete user_path(user)
+    end
 
-            before do
-                login admin
-                visit users_path
-            end
-
-            it { should have_link('delete', href: user_path(user)) }
-            it { should_not have_link('delete', href: user_path(admin)) }
-
-            describe "redirects properly", type: :request do
-                before do
-                    login admin, avoid_capybara: true
-                    delete user_path(user)
-                end
-
-                specify { expect(response).to redirect_to(users_path) }
-            end
+    specify { expect(response).to redirect_to(users_path) }
+      end
 
       it "produces a delete message" do
-	click_link('delete', match: :first)
-	should have_alert(:success)
+    click_link('delete', match: :first)
+    should have_alert(:success)
       end
 
-            it "removes a user from the system" do
-                expect { click_link('delete', match: :first) }.to change(User, :count).by(-1)
-            end
-        end
+      it "removes a user from the system" do
+        expect { click_link('delete', match: :first) }.to change(User, :count).by(-1)
+      end
     end
+  end
 end
+
