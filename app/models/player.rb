47a472d3b6a9dc129bdcdf40a882f7ba09040910
Justin Southworth <justin_southworth@taylor.edu>
2013-10-31 12:27:55 -0400

    Passing all but the polymorphic stuff.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,9 @@
 class Player < ActiveRecord::Base
   belongs_to :user
   belongs_to :contest
-  has_many :matches, through: :player_matches
+
+  has_many :matches, through: :player_matches, as: :manager
+
+  validates :contest,   presence: true 
+  validates :user,      presence: true 
 end
