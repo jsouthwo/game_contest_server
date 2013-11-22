d3aceac23d021daefdc725a1cc6a6711b4cbd179
Justin Southworth <justin_southworth@taylor.edu>
2013-11-22 12:46:24 -0500

    Uploadable concern. 60. Not sure where the extra 2 came from. 60.

---
+++ app/models/concerns/uploadable.rb
@@ -0,0 +1,32 @@
+module Uploadable
+    extend ActiveSupport::Concern
+
+    included do 
+        validates :file_location, presence: true
+    validate :valid_file_location?
+    end
+
+    def valid_file_location?
+        errors.add(:file_location, "must be valid") if 
+        self.file_location and not (File.exists?(self.file_location) or File.symlink?(self.file_location))
+    end
+
+    def upload=(uploaded_io)
+        time_no_spaces = Time.now.to_s.gsub(/\s/, '_')
+        # ... code and stuff ...
+        #
+        if uploaded_io.nil?
+            # problem -- deal with later
+            'lala'
+        else
+            file_location = Rails.root.join('code',
+                                            self.class.to_s.pluralize.downcase,
+                                            Rails.env,
+                                            time_no_spaces + SecureRandom.hex
+                                           ).to_s
+            IO::copy_stream(uploaded_io, file_location)
+            self.file_location = file_location
+        end
+    end
+
+end
--- app/models/referee.rb
+++ app/models/referee.rb
@@ -5,6 +5,8 @@ class Referee < ActiveRecord::Base
     has_many :contests
     has_many :matches, as: :manager
 
+    include Uploadable
+=begin
     def upload=(uploaded_io)
         time_no_spaces = Time.now.to_s.gsub(/\s/, '_')
         # ... code and stuff ...
@@ -22,11 +24,13 @@ class Referee < ActiveRecord::Base
             self.file_location = file_location
         end
     end
+=end
 
     validates :name,                presence: true, uniqueness: true
     validates :rules_url,           presence: true, url: true
-#    validates :rules_url,           presence: true, format: {with: URI::regexp}
+# OR format: {with: URI::regexp}
     validates :players_per_game,    presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to:10} 
+=begin
     validates :file_location,       presence: true
     validate :valid_file_location?
 
@@ -34,4 +38,5 @@ class Referee < ActiveRecord::Base
         errors.add(:file_location, "must be valid") if 
         self.file_location and not (File.exists?(self.file_location) or File.symlink?(self.file_location))
     end
+=end
 end
