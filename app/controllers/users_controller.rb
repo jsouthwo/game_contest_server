9ba3ed2250995e66bf3dda60e88b289a368a5a43
Justin Southworth <justin_southworth@taylor.edu>
2013-11-25 12:41:23 -0500

    Starting using json. 454/454. 0.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-    before_action only: [:new, :create] do
-        ensure_user_not_logged_in__flash_warn_goes_to_root
-    end
+    before_action :ensure_user_not_logged_in__flash_warn_goes_to_root, only: [:new, :create]
 
     before_action only: [:edit, :update] do
         unless ensure_user_logged_in__flash_warn_goes_to_login
@@ -9,15 +7,24 @@ class UsersController < ApplicationController
         end
     end
 
+    
+    before_action :ensure_admin__flash_danger_goes_to_root, only: [:destroy]
+
+=begin
     before_action only: [:destroy] do
         ensure_admin__flash_danger_goes_to_root
     end
+=end
+
+    respond_to :html, :json, :xml
 
     def new
         @user = User.new
+        respond_with(@user)
     end
 
-
+=begin
+For documentation purposes
     def create
         @user = User.new(acceptable_params)
 
@@ -29,10 +36,21 @@ class UsersController < ApplicationController
             render :new
         end
     end
+=end
 
+    def create
+        @user = User.new(acceptable_params)
+
+        if @user.save
+            flash[:success] = "Welcome to the site, #{@user.username}."
+            login(@user)
+        end
+        respond_with(@user)
+    end
 
     def edit
         @user = User.find(params[:id])
+        respond_with(@user)
     end
 
 
@@ -40,10 +58,8 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(acceptable_params)
             flash[:success] = "Successfully updated your account, #{@user.username}."
-            redirect_to user_path
-        else
-            render :edit
         end
+        respond_with(@user)
     end
 
 
@@ -54,8 +70,8 @@ class UsersController < ApplicationController
 
     def index
         @users = User.all
+        respond_with(@users)
     end
-
 
     def destroy
         @user = User.find(params[:id])
@@ -69,6 +85,20 @@ class UsersController < ApplicationController
         end
     end
 
+=begin
+    def destroy
+        @user = User.find(params[:id])
+        # TODO: Move this into a before action to allow the danger flash to work.
+        if current_user?(@user) then # Don't let admin delete self
+            flash[:danger] = "Admin may not delete self."
+            redirect_to root_path
+        else
+            flash[:success] = "#{@user.username} deleted"
+            @user.destroy
+        end
+        respond_with(@user)
+    end
+=end
 
 
     private
