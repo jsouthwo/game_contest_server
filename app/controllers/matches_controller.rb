c32741d643e73ba21c74c4ba7e3dcb522ef69035
Justin Southworth <justin_southworth@taylor.edu>
2013-12-02 12:57:11 -0500

    Match stuff. 22.

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,12 @@
 class MatchesController < ApplicationController
+    ## Aren't currently supporting all actions
+    def show
+        @match = Match.find(params[:id])
+    end
+
+    def index
+        ## Probably not correct
+        @matches = Match.all
+    end
+
 end
