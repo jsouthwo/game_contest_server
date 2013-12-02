f18185e2a5695797c236962088c418d48d2ba51f
Justin Southworth <justin_southworth@taylor.edu>
2013-12-02 13:21:23 -0500

    Mas changes. 19.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,11 +1,19 @@
 class Match < ActiveRecord::Base
-  belongs_to :manager, polymorphic: true
+    belongs_to :manager, polymorphic: true
 
-  has_many :players, through: :player_matches
-  has_many :player_matches
+    has_many :players, through: :player_matches
+    has_many :player_matches
 
-  validates :manager, presence: true
+    validates :manager, presence: true
 
-  validates :completion, presence: true
-#  validates :earliest_start, presence: true
+    #  validates :earliest_start, presence: true
+    validates :status, presence: true
+    validates :completion, presence: true
+=begin NOT SURE HOW TO DO THIS
+    puts :status
+    if :status.to_s == "Completed"
+        validates :completion, timeliness: { on_or_before: :now }
+    end
+=end
+
 end
