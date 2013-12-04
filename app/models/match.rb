52968e66d3da30a7ddbff674142e66fb3f962069
Justin Southworth <justin_southworth@taylor.edu>
2013-12-04 08:59:34 -0500

    Done! 508/508. 0 failures.

--- app/models/match.rb
+++ app/models/match.rb
@@ -5,17 +5,18 @@ class Match < ActiveRecord::Base
     has_many :player_matches
 
     validates :manager,         presence: true
-
     validates :status,          presence: true
     validates :earliest_start,  presence: true, allow_blank: true
     validates :earliest_start,  timeliness: { type: :date, on_or_after: :now }, if: :waiting?
     validates :completion,      presence: true
     validates :completion,      timeliness: { type: :date, on_or_before: :now }, if: :completed?
-#    validates :players, presence: true
+
     validate :correct_number_of_players
 
     def correct_number_of_players
-        errors.add('players.count', "must equal " + manager.referee.players_per_game.to_s)
+        if manager
+            errors.add('players.count', "must equal " + manager.referee.players_per_game.to_s) unless players.count == manager.referee.players_per_game
+        end
     end
 
     def completed?
